325c69a477a9f0b86cb31a06a8e4eb70a0822d84
Norme-Zvenyika <noz224@lehigh.edu>
2024-12-03 00:23:06 -0500

    add the base url for the backend

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@
 export const API_CONFIG = {
     // base urls for different apis
     URLS: {
-        LOCAL_API: 'http://localhost:3001',
+        LOCAL_API: 'https://3efgsfe513.execute-api.us-east-1.amazonaws.com',
         OFFICE_API: 'https://theofficeapi.dev/api'
     },
 
